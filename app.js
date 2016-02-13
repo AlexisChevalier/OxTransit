0a39a8cfb97e958ceabbe40113d2fbe838c078ed
Alexis Chevalier <alexis.chevalier1@gmail.com>
2016-02-13 12:28:45 +0000

    Removed last tab and updated icon

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
